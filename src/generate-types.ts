224497603806ddb25b9498b90759e21d7cf7e2e3
David Farber <david+github@stedi.com>
2020-06-19 17:57:16 -0600

    Implement all ports (api endpoints).

--- src/generate-types.ts
+++ src/generate-types.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import { MessageStreamFactory } from "./generate-util/message-stream";
 import { TypeEmitter } from "./generate-util/type-emitter";
 
-const mappingsDir = `${__dirname}/netsuite-mappings/`;
+const mappingsDir = `${__dirname}/netsuite_webservices/2019_2/__mappings/`;
 const mappingModules = fs.readdirSync(mappingsDir, { withFileTypes: true })
   .filter(item => !item.isDirectory())
   .filter(item => item.name.startsWith("com_netsuite_webservices_"))
@@ -10,7 +10,7 @@ const mappingModules = fs.readdirSync(mappingsDir, { withFileTypes: true })
   .map(item => item.name.replace(/\.js$/, ""));
 // const mappingModules = ["com_netsuite_webservices_platform_faults_2019_2"];
 
-const emitter = new TypeEmitter(`${__dirname}/netsuite-mappings/`);
+const emitter = new TypeEmitter(mappingsDir);
 
 const typesDir = `${__dirname}/netsuite_webservices/2019_2/`;
 // mappingModules.forEach(mappingName => emitter.emitModule(mappingName, MessageStreamFactory.stdoutFactory(typesDir)));
