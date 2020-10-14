64c91bd73b7e9995d2f7776fdff3978ef85f4c1a
Eno Compton <eno@stedi.com>
2020-10-14 10:54:49 -0600

    docs: add documentation to soap types

--- src/soap-types.ts
+++ src/soap-types.ts
@@ -1,3 +1,10 @@
+/**
+ * A base class that records the TYPE_NAME for a given subclass based on
+ * the mappings name captured by propsWithMappingsName.
+ *
+ * jsonix uses TYPE_NAME to find the associated mappings, such that the JS
+ * class can be serialized back into XML.
+ */
 export class SoapMappingBase {
   constructor(props: { MAPPINGS_NAME: string }) {
     Object.assign(this, {
@@ -6,6 +13,9 @@ export class SoapMappingBase {
   }
 }
 
+/**
+ * Returns the provided props with the passed in mappings name.
+ */
 export function propsWithMappingsName<T>(
   props: T,
   mappingsName: string
